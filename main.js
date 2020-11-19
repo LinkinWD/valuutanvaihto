a3effcfaa3b479c2b6d0be5e87dd73a6ef3b31da
unknown <yaramodesu@gmail.com>
2020-11-19 13:57:03 +0200

    api toimii, objecti muutettu arraysks

---
+++ main.js
@@ -0,0 +1,17 @@
+let kurssit = []
+  
+  axios.get('https://v6.exchangerate-api.com/v6/204dbf8d6253ed46c8c4c8a0/latest/EUR').then(function (res) {
+      console.log(res.data);
+      let vastaus = res.data.conversion_rates
+      
+      let kurssit = Object.entries(vastaus);
+      console.log(kurssit)
+      console.log(`Punta: ${kurssit[17]}`)
+  }).catch(function (error) {
+      console.error(error);
+  });
+  
+
+
+
+  
